18a9f81da444a6d07e062571975a2d7c4f394e97
sunhaixin <1113285307@qq.com>
2019-01-16 01:12:02 +0800

    WhiteSpaceExample

---
+++ page/components/Layout/WhiteSpace/WhiteSpaceExample.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Basic from '../Basic';
+import { WhiteSpace } from '@ant-design/react-native';
+const PlaceHolder = props => (
+  <View
+    style={{
+      backgroundColor: '#eee',
+      height: 30,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+    {...props}
+  >
+    <Text style={{ color: '#bbb' }}>Block</Text>
+  </View>
+);
+export default class WhiteSpaceExample extends React.Component {
+  render() {
+    return (
+      <View>
+        <Basic/>
+        <WhiteSpace size="xs" />
+        <PlaceHolder />
+
+        <WhiteSpace size="sm" />
+        <PlaceHolder />
+
+        <WhiteSpace />
+        <PlaceHolder />
+
+        <WhiteSpace size="lg" />
+        <PlaceHolder />
+
+        <WhiteSpace size="xl" />
+        <PlaceHolder />
+      </View>
+    );
+  }
+}
