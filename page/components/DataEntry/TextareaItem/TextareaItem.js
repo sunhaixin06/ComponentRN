d89add8ea1c907a4825d7ab98975c589bbe97441
sunhaixin <1113285307@qq.com>
2019-01-19 16:59:00 +0800

    TextAreaItem组件

---
+++ page/components/DataEntry/TextareaItem/TextareaItem.js
@@ -0,0 +1,51 @@
+/* tslint:disable:no-console */
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import { List, TextareaItem } from '@ant-design/react-native';
+export default class BasicTextAreaItemExample extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = val => {
+      // console.log(val);
+      this.setState({ val });
+    };
+    this.state = {
+      val: '默认带value',
+    };
+  }
+  render() {
+    return (
+      <ScrollView
+        style={{ flex: 1 }}
+        automaticallyAdjustContentInsets={false}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+      >
+        <List renderHeader={'基本'}>
+          <TextareaItem rows={4} placeholder="固定行数" />
+
+          <TextareaItem rows={4} placeholder="多行带计数" count={100} />
+
+          <TextareaItem
+            rows={4}
+            placeholder="高度自适应"
+            autoHeight
+            style={{ paddingVertical: 5 }}
+          />
+
+          <TextareaItem value={this.state.val} onChange={this.onChange} />
+
+          <TextareaItem value="不可编辑 editable = {false}" editable={false} />
+
+          <TextareaItem clear={false} placeholder="不显示清除按钮" />
+
+          <TextareaItem
+            error
+            defaultValue="报错样式 error={true}"
+            onErrorClick={() => console.log('err')}
+          />
+        </List>
+      </ScrollView>
+    );
+  }
+}
