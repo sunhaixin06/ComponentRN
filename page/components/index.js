744eb692b95961a9861fb43a1bc156b33420d14f
sunhaixin <1113285307@qq.com>
2019-01-19 16:53:14 +0800

    所有组件的导出

---
+++ page/components/index.js
@@ -0,0 +1,32 @@
+//Layout
+import { Flex_ } from '../components/Layout/Flex';
+import { WingBlank_ } from '../components/Layout/WingBlank';
+import { WhiteSpace_} from '../components/Layout/WhiteSpace';
+//Navigation
+import { Drawer_ } from '../components/Navigation/Drawer'; 
+import { Tabs_ } from '../components/Navigation/Tabs'; 
+import { TabBar_ } from '../components/Navigation/TabBar'; 
+import { Popover_ } from '../components/Navigation/Popover'; 
+import { Pagination_ } from '../components/Navigation/Pagination'; 
+import { SegmentedControl_ } from '../components/Navigation/SegmentedControl'; 
+
+//DataEntry
+import { Button_ } from '../components/DataEntry/Button';
+import { Radio_ } from '../components/DataEntry/Radio';
+import { Slider_ } from '../components/DataEntry/Slider';
+import { Stepper_} from '../components/DataEntry/Stepper';
+import { Switch_ } from '../components/DataEntry/Switch';
+import { SearchBar_ } from '../components/DataEntry/SearchBar';
+import { Checkbox_ } from '../components/DataEntry/CheckBox';
+import { DatePicker_ } from '../components/DataEntry/DatePicker';
+import { DatePickerView_ } from '../components/DataEntry/DatePickerView';
+import { InputItem_ } from '../components/DataEntry/InputItem';
+import { PickerView_ } from '../components/DataEntry/PickerView';
+import { TextareaItem_ } from '../components/DataEntry/TextareaItem';
+
+
+export const routers =  {
+    Flex_, WingBlank_, WhiteSpace_, //Layout 
+    Drawer_, Tabs_, TabBar_, Popover_, Pagination_, SegmentedControl_, //Navigation
+    Button_, Radio_, Slider_, Stepper_, Switch_, SearchBar_,  Checkbox_, DatePicker_, DatePickerView_, InputItem_, PickerView_, TextareaItem_ //DataEntry
+};
