b1323553d79acb319b0003b2c00df2fca6661c63
sunhaixin <1113285307@qq.com>
2019-01-22 12:09:57 +0800

    SwipeAction组件

---
+++ page/components/Gesture/SwipeAction/SwipeAction.js
@@ -0,0 +1,50 @@
+/* tslint:disable:no-console */
+import React from 'react';
+import { View } from 'react-native';
+import { List, SwipeAction } from '@ant-design/react-native';
+export default class BasicSwipeActionExample extends React.Component {
+  render() {
+    const right = [
+      {
+        text: 'More',
+        onPress: () => console.log('more'),
+        style: { backgroundColor: 'orange', color: 'white' },
+      },
+      {
+        text: 'Delete',
+        onPress: () => console.log('delete'),
+        style: { backgroundColor: 'red', color: 'white' },
+      },
+    ];
+    const left = [
+      {
+        text: 'Read',
+        onPress: () => console.log('read'),
+        style: { backgroundColor: 'blue', color: 'white' },
+      },
+      {
+        text: 'Reply',
+        onPress: () => console.log('reply'),
+        style: { backgroundColor: 'green', color: 'white' },
+      },
+    ];
+    return (
+      <View style={{ flex: 1, backgroundColor: '#f5f5f9' ,  marginTop: 30}}>
+        <List>
+          <SwipeAction
+            autoClose
+            style={{ backgroundColor: 'transparent' }}
+            right={right}
+            left={left}
+            onOpen={() => console.log('open')}
+            onClose={() => console.log('close')}
+          >
+            <List.Item extra="extra content">
+              Simple example: left and right buttons
+            </List.Item>
+          </SwipeAction>
+        </List>
+      </View>
+    );
+  }
+}
